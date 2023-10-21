1902941a0a5f0935dd14e02086bc5b67dab1cedf
Aashu1303 <aashutoshag03@gmail.com>
2023-10-21 19:52:44 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.383e64b7b55ad40793098c157ec0f3b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78cd708816187c0b0c0bec18217900e",
+    "url": "/react-task-1/index.html"
+  },
+  {
+    "revision": "b02f9f1e4a6d1b31ab67",
+    "url": "/react-task-1/static/css/main.345b5c5e.chunk.css"
+  },
+  {
+    "revision": "7e2b530b02ead0fd9ff9",
+    "url": "/react-task-1/static/js/2.536c0f80.chunk.js"
+  },
+  {
+    "revision": "15a50c1c2b2805fa91df",
+    "url": "/react-task-1/static/js/3.e92f44bb.chunk.js"
+  },
+  {
+    "revision": "b02f9f1e4a6d1b31ab67",
+    "url": "/react-task-1/static/js/main.23723b08.chunk.js"
+  },
+  {
+    "revision": "efba97710e8352c3384a",
+    "url": "/react-task-1/static/js/runtime~main.6bdb6e15.js"
+  }
+]);
